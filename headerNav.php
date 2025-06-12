2280f38aed7e05036de1f5107f40d45b9aae2a62
Sadé <161801628+Sadeeee-n@users.noreply.github.com>
2025-06-12 21:00:54 +0200

    Add files via upload

--- headerNav.php
+++ headerNav.php
@@ -1,73 +1,94 @@
-<?php  session_start();
-	include_once 'includes/config.php';
-	//  all functions
-	require_once 'functions/functions.php';
-
-	//run whenever this file is used no need of isset or any condition to get website image footer etc
-	$sql5 ="SELECT * FROM  settings;";
-
-	$result5 = $conn->query($sql5);
-	$row5 = $result5->fetch_assoc();
-	$_SESSION['web-name'] = $row5['website_name'];
-	$_SESSION['web-img'] = $row5['website_logo'];
-	$_SESSION['web-footer'] = $row5['website_footer'];
-
-	// TODO: Also update setting to add info email, whatsapp, address etc to change it dynamically
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
-  <head>
-  	<meta charset="UTF-8" />
-  	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>Admin-Panel</title>
+    <!-- <link rel="stylesheet" href="../css/style.php"> -->
+    <!--Bootstrap css link-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <!--Bootstrap js link-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!--fontawesome link--> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
-	<!-- Favicon -->
-	<link rel="shortcut icon" href="./images/flogo/fav.png" type="image/x-icon" />
-	
-    <title><?php echo $_SESSION['web-name']; ?></title>
-
-	
-
-
-	<!-- <link href="./css/font-awesome.css" rel="stylesheet" type="text/css"> -->
-	<!-- font awesome code -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	
-	<!-- main style -->
-	<!-- <link rel="stylesheet" href="./css/style.php"> -->
-	
-	<!--
-		- custom css link
-	-->
-	
-	<link rel="stylesheet" type="text/css" href="./css/style-prefix.css?<?php echo time(); ?>" />
-	<!-- <link rel="stylesheet" href="./css/style-prefix.css" /> -->
-	<link rel="stylesheet" type="text/css" href="./css/view-details.css?<?php echo time(); ?>" />
-	<link rel="stylesheet" type="text/css" href="./css/cart-card-design.css?<?php echo time(); ?>" />
-	<link rel="stylesheet" type="text/css" href="./css/aboutus.css?<?php echo time(); ?>" />
-	<link rel="stylesheet" type="text/css" href="./css/contact.css?<?php echo time(); ?>" />
-	<link rel="stylesheet" type="text/css" href="./css/pagination.css?<?php echo time(); ?>" />
-	<!--
-		- google font link
-	-->
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-
-  </head>
-
+<style>
+        * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+      }
+      .text-white > h6:hover {
+        color: #f2f2f2;
+      }
+</style>
+</head>
 <body>
 
-<!-- added manually to some places like contact and about page and footer -->
-<?php
-  //  site details
-    // $site_name = "HCA E-Commerce";
-    $site_address = "5900 BALCONES DRIVE STE 4129 AUSTIN TX, USA 78731";
-    $site_contact_num = "(336) 717-5310";
-    $site_info_email = "info@clickableshop.com";
-?>
+    <nav
+      class="navbar navbar-expand-lg bg-body-tertiary bg-primary"
+      style="text-transform: uppercase"
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" href="../index.php">
+          <h2>Store</h2>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link active text-white"
+                aria-current="page"
+                href="post.php"
+              >
+                <h6>Admin Panel</h6>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active text-white" href="post.php">
+                <h6>Post</h6>
+              </a>
+            </li>
+            <li class="nav- text-white">
+              <a class="nav-link active text-white" href="catagory.php">
+                <h6>Catagory</h6>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active text-white" href="users.php">
+                <h6>Users</h6>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active text-white" href="settings.php">
+                <h6>Setting</h6>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active text-white" href="logout.php?">
+                <h6>Logout</h6>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</body>
+</html>
